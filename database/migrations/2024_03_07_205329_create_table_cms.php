a8990103e155f7e201d865524464fed5fc049df4
Ariesta <dias.saiman@ag-it.com>
2024-03-08 14:55:39 +0700

    hotfix/any

---
+++ database/migrations/2024_03_07_205329_create_table_cms.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cms', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand_image')->nullable()->default(null);
+            $table->string('brand_text')->nullable()->default(null);
+            $table->string('footer_text_left')->nullable()->default(null);
+            $table->string('footer_text_middle')->nullable()->default(null);
+            $table->string('footer_text_right')->nullable()->default(null);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cms');
+    }
+};
